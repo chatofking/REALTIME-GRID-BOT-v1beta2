b895deec7c73db3fa4b0c2df1a0375409073a4bc
chatofking <chat_4432@hotmail.com>
2022-12-07 20:16:36 +0700

    first commit

---
+++ debug.js
@@ -0,0 +1,32 @@
+const API_KEY = 'AK0BLAL5EWWN165HZRY4';
+const SECRET_KEY = 'igZjlMcuuX7fFiwEiUN2BrNtIpyOSwuK8kBTb3qo';
+
+var start = new Date(Date.now() - (7200 * 1000)).toISOString();
+
+console.log(start);
+//https://data.alpaca.markets/v1beta1/crypto/ETHUSD/bars?exchanges=CBSE&timeframe=1Min&start=
+//https://data.alpaca.markets/v1beta2/crypto/bars?symbols=ETH/USD&timeframe=1Min&start=
+var bars_url = 'https://data.alpaca.markets/v1beta2/crypto/bars?symbols=ETH/USD&timeframe=1Min&start=' + start;
+
+fetch(bars_url, {
+    headers: {
+        'APCA-API-KEY-ID': API_KEY,
+        'APCA-API-SECRET-KEY': SECRET_KEY
+    }
+}).then((r) => r.json())
+    .then((response) => {
+        console.log(response);
+          
+        for (const key in response.bars) {
+            const bar = response.bars[key];
+            const data = bar.map(b => (
+                {
+                    open: b.o,
+                    high: b.h,
+                    low: b.l,
+                    close: b.c,
+                    time: Date.parse(b.t) / 1000
+                }
+            ));
+            console.log(data);
+        }})
